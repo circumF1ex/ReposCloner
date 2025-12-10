973d3fdad49d979586c35e3d58be74ca63cc5440
DanilSizz <91390208+circumF1ex@users.noreply.github.com>
2025-12-10 20:58:01 +0300

    added all repos

--- start.bat
+++ start.bat
@@ -1,5 +1,10 @@
 @echo off
 chcp 65001
+echo Проверка и установка зависимостей...
+for /f %%i in (Зависимости) do (
+    echo Установка %%i...
+    python -m pip install %%i
+)
 echo Запуск программы для клонирования и обновления репозиториев...
 python main.py repos.txt
 echo Готово! Проверьте папку ./repos и файл changes_results.json.
